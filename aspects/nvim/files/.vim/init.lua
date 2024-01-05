09974a826c37e03aa2460b90afa97c11e5f8eb40
Daniel Moura <daniel.moura@loft.com.br>
2024-01-05 16:18:26 -0300

    feat: init.lua

---
+++ aspects/nvim/files/.vim/init.lua
@@ -0,0 +1,12 @@
+vim.keymap.set('n', ';', ':')
+vim.keymap.set('n', ':', ';')
+require'nvim-treesitter.configs'.setup {
+  highlight = {
+    enable = true,
+    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    -- Instead of true it can also be a list of languages
+    additional_vim_regex_highlighting = false,
+  },
+}
